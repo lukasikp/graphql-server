2137ee2ea6837ff223f49ab44236dd109f470982
lukasikp <lukasik.paulina@gmail.com>
2019-07-07 15:25:52 +0200

    add projet data, notrealdb, jwt

--- server.js
+++ server.js
@@ -1,25 +1,48 @@
 const { ApolloServer, gql } = require("apollo-server-express");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 const express = require("express");
+const expressJwt = require("express-jwt");
+const jwt = require("jsonwebtoken");
+const db = require("./db");
 
 const port = 9000;
+const jwtSecret = Buffer.from("Zn8Q5tyZ/G1MHltc4F/gTkVJMlrbKiZt", "base64");
 
 const typeDefs = gql`
   type Query {
     greeting: String
   }
 `;
+
 const resolvers = {
   Query: {
     greeting: () => "Hello World"
   }
 };
-
 const server = new ApolloServer({ typeDefs, resolvers });
 
 const app = express();
+app.use(
+  cors(),
+  bodyParser.json(),
+  expressJwt({
+    secret: jwtSecret,
+    credentialsRequired: false
+  })
+);
 const path = "/graphql";
-
 server.applyMiddleware({ app, path });
 
-app.listen(port, () => console.log(`server is running on port ${port}`));
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  const user = db.users.list().find(user => user.email === email);
+  if (!(user && user.password === password)) {
+    res.sendStatus(401);
+    return;
+  }
+  const token = jwt.sign({ sub: user.id }, jwtSecret);
+  res.send({ token });
+});
+
+app.listen(port, () => console.info(`Server started on port ${port}`));
